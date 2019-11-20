fd240a58c0a8dabbdb0b27f847d865d9785f6d7e
Chris Arderne <chris@rdrn.me>
2019-11-20 09:26:07 +0100

    Update to work better with systemctl

--- button.py
+++ button.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+
 import RPi.GPIO as GPIO
 
 import lights
@@ -10,19 +12,21 @@ def button_callback(channel):
     lights.sleep(45)
 
 
-pin = 17
+def setup_listener():
+    pin = 26
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(pin, GPIO.RISING, callback=button_callback)
+    while True:
+        time.sleep(0.1)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(pin, GPIO.RISING, callback=button_callback)
-
-try:
-    message = input("Press enter to quit\n\n")
-except KeyboardInterrupt:
-    pass
-finally:
-    lights.cleanup()
-
-GPIO.cleanup()
+if __name__ == "__main__":
+    try:
+        setup_listener()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
+        lights.cleanup()
